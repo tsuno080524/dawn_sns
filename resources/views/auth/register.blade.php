2c4cfdb076d89dda1592a0d0b68eaec642619c97
tsunoda <tsunoda080524@gmail.com>
2023-03-20 12:48:39 +0900

    新規登録　表記の変更

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,16 +6,16 @@
 
 <h2>新規ユーザー登録</h2>
 
-{{ Form::label('ユーザー名') }}
+{{ Form::label('UserName') }}
 {{ Form::text('username',null,['class' => 'input']) }}
 
-{{ Form::label('メールアドレス') }}
+{{ Form::label('MailAddress') }}
 {{ Form::text('mail',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード') }}
+{{ Form::label('Password') }}
 {{ Form::text('password',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード確認') }}
+{{ Form::label('Password confirm') }}
 {{ Form::text('password-confirm',null,['class' => 'input']) }}
 
 {{ Form::submit('登録') }}
